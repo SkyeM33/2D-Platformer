691eab59590989601a64dd76b2952cdd1c4e739f
SkyeM33 <142693429+SkyeM33@users.noreply.github.com>
2023-10-31 13:48:44 -0400

    Enemy deaths

--- Enemies/boar_3.gd
+++ Enemies/boar_3.gd
@@ -1,18 +1,36 @@
 extends CharacterBody2D
 
 var Player = get_node_or_null("/root/Game/Player")
+@onready var SM = $StateMachine
 
 func ready():
 	$Boar.animation = "Idle"
 
 
 func _on_above_body_entered(body):
-	queue_free()
+	SM.set_state("Dying")
+	$Boar.animation = "Dying"
 
 
 func _on_body_body_entered(body):
 	if body.name != "Boar 3":
 		body.die()
 		$Boar.animation = "Attacking"
+		SM.set_state("Attack")
+		
+
 		
 		
+
+		
+
+
+func _on_boar_animation_finished():
+	if SM.state_name == "Attack":
+		$Boar.animation = "Idle"
+		SM.set_state("Move")
+	if SM.state_name == "Move":
+		$Boar.animation = "Idle"
+	if SM.state_name == "Dying":
+		queue_free()
+	
